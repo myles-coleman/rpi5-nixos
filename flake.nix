98c8b11ee18208e3b2ce815002b8b64f5545a9d4
Myles Coleman <mylescoleman05@gmail.com>
2026-03-31 13:44:15 -0700

    chore: use cloudflare dns

--- flake.nix
+++ flake.nix
@@ -88,7 +88,7 @@
             prefixLength = 24;
           }];
           networking.defaultGateway = "10.0.0.1";
-          networking.nameservers = [ "10.0.0.1" ];
+          networking.nameservers = "1.1.1.1";
         })
       ];
     };
@@ -105,7 +105,7 @@
             prefixLength = 24;
           }];
           networking.defaultGateway = "10.0.0.1";
-          networking.nameservers = [ "10.0.0.1" ];
+          networking.nameservers = [ "1.1.1.1" ];
         })
       ];
     };
@@ -122,7 +122,7 @@
             prefixLength = 24;
           }];
           networking.defaultGateway = "10.0.0.1";
-          networking.nameservers = [ "10.0.0.1" ];
+          networking.nameservers = "1.1.1.1";
         })
       ];
     };
@@ -139,7 +139,7 @@
             prefixLength = 24;
           }];
           networking.defaultGateway = "10.0.0.1";
-          networking.nameservers = [ "10.0.0.1" ];
+          networking.nameservers = "1.1.1.1";
         })
       ];
     };
